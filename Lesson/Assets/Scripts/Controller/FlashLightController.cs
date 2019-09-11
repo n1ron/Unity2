fc7395a1cfca0a985c8c0373687e2eae5c0164f5
n1ron <48497211+n1ron@users.noreply.github.com>
2019-09-12 02:37:22 +0300

    Rebuilded from scratch

---
+++ Lesson/Assets/Scripts/Controller/FlashLightController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+namespace Geekbrains
+{
+	public sealed class FlashLightController : BaseController, IOnUpdate, IInitialization
+	{
+		private FlashLightModel _flashLightModel;
+		private FlashLightUi _flashLightUi;
+
+		public void Init()
+		{
+			_flashLightModel = Object.FindObjectOfType<FlashLightModel>();
+			_flashLightUi = Object.FindObjectOfType<FlashLightUi>();
+		}
+
+		public override void On()
+		{
+			if(IsActive) return;
+			if (_flashLightModel == null) return;
+			if (_flashLightUi == null) return;
+			if (_flashLightModel.BatteryChargeCurrent <= 0) return;
+			base.On();
+			_flashLightModel.Switch(true);
+		}
+
+		public override void Off()
+		{
+			if (!IsActive) return;
+			base.Off();
+			_flashLightModel.Switch(false);
+		}
+
+		public void OnUpdate()
+		{
+            if (_flashLightModel.ShowBatteryLevel() < 1) _flashLightUi.SetActive(true);
+            else _flashLightUi.SetActive(false);
+
+            _flashLightUi.FillAmount = _flashLightModel.ShowBatteryLevel();
+
+            if (!IsActive)
+            {
+                _flashLightModel.ChargeBattery();                
+                return;
+            }
+			_flashLightModel.Rotation();
+			if (_flashLightModel.SpendBattery())
+			{
+            }
+			else
+			{
+				Off();
+			}
+        }
+	}
+}
---
+++ Lesson/Assets/Scripts/View/FlashLightUi.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Geekbrains
+{
+    public sealed class FlashLightUi : MonoBehaviour
+    {
+	    private Image _image;
+
+	    private void Awake()
+	    {
+		    _image = GetComponent<Image>();
+	    }
+
+	    public float FillAmount
+	    {
+		    set => _image.fillAmount = value;
+	    }
+
+	    public void SetActive(bool value)
+	    {
+		    _image.gameObject.SetActive(value);
+	    }
+    }
+}
