4652c3b388b49f025135aae0962a8bc83add07e5
n1ron <48497211+n1ron@users.noreply.github.com>
2019-09-13 15:50:19 +0300

    Manual revert

---
+++ Lesson_2v3/Assets/Scripts/Model/Inventory.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+namespace Geekbrains
+{
+	public sealed class Inventory : IInitialization
+	{
+		private Weapon[] _weapons = new Weapon[5];
+
+		public Weapon[] Weapons => _weapons;
+
+		public FlashLightModel FlashLight { get; private set; }
+
+		public void OnStart()
+		{
+			_weapons = Main.Instance.Player.GetComponentsInChildren<Weapon>();
+
+			foreach (var weapon in Weapons)
+			{
+				weapon.IsVisible = false;
+			}
+
+			FlashLight = Object.FindObjectOfType<FlashLightModel>();
+		}
+
+        //todo Добавить функционал
+
+        public void SelectWeapon(int i)
+        {
+            Main.Instance.WeaponController.Off();
+            var tempWeapon = Weapons[i];
+            if (tempWeapon != null)
+            {
+                Main.Instance.WeaponController.On(tempWeapon);
+            }
+        }
+
+        public void SelectNextWeapon()
+        {
+            int tempI = System.Array.IndexOf(Weapons, Main.Instance.WeaponController.Weapon);
+            if (tempI == Weapons.Length - 1) SelectWeapon(0);
+            else SelectWeapon(tempI + 1);
+        }
+        
+        public void SelectPreviousWeapon()
+        {
+            int tempI = System.Array.IndexOf(Weapons, Main.Instance.WeaponController.Weapon);
+            if (tempI == 0) SelectWeapon(Weapons.Length - 1);
+            else SelectWeapon(tempI - 1);
+        }
+
+
+        public void RemoveWeapon(Weapon weapon)
+        {
+            
+        }
+
+        private void SwitchWeapon()
+        {
+
+        }
+    }
+}
