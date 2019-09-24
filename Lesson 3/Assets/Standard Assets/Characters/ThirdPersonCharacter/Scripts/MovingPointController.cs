9d99e2aa735e5e4b75b6ec793c4a2d675178e964
n1ron <48497211+n1ron@users.noreply.github.com>
2019-09-24 16:53:12 +0300

    HomeWork 3 added

---
+++ Lesson 3/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/MovingPointController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+    public class MovingPointController : MonoBehaviour
+    {
+        private MovingPoints _movingPoints;
+        private List<AICharacterControl> _listOfColliders;
+        
+        void Start()
+        {
+            _movingPoints = Camera.main.GetComponent<MovingPoints>();
+            _listOfColliders = new List<AICharacterControl>();
+            
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            var temp = collision.gameObject.GetComponent<AICharacterControl>();
+            temp.DequeueTarget();
+            _listOfColliders.Remove(temp);
+            if (_listOfColliders.Count == 0) Destroy(gameObject);
+        }
+
+        public void GetList(List<AICharacterControl> list)
+        {
+            foreach (var ai in list)
+            {
+                if (ai != null) _listOfColliders.Add(ai);
+            }
+        }
+    }
+}
---
+++ Lesson 3/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/MovingPoints.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+    public class MovingPoints : MonoBehaviour
+    {
+        [SerializeField] private Transform _agent;
+        [SerializeField] private Transform _point;
+        private Queue<Vector3> _points = new Queue<Vector3>();
+        private readonly Color _color = Color.red;
+        private readonly int lengthOfLineRenderer = 2;
+        private LineRenderer _lineRenderer;
+        private Camera _camera;
+        [HideInInspector] public Transform _target;
+        [HideInInspector] public List<AICharacterControl> ListAI;
+
+        private void Start()
+        {
+            _camera = FindObjectOfType<Camera>();
+            _lineRenderer = new GameObject("LineRenderer").AddComponent<LineRenderer>();
+            _lineRenderer.startWidth = 0.5F;
+            _lineRenderer.endWidth = 0.5F;
+            _lineRenderer.material = new Material(Shader.Find("Mobile/Particles/Additive"));
+            _lineRenderer.startColor = _color;
+            _lineRenderer.endColor = _color;
+            _lineRenderer.positionCount = lengthOfLineRenderer;
+            _lineRenderer.SetPosition(0, _agent.position);
+            ListAI = new List<AICharacterControl>();
+        }
+
+        private void Update()
+        {
+            if (Physics.Raycast(_camera.ScreenPointToRay(Input.mousePosition), out var hit))
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    DrawPoint(hit.point);
+                }
+            }
+            _lineRenderer.SetPosition(1, hit.point);
+        }
+
+        private void DrawPoint(Vector3 position)
+        {
+            var tempPoint = Instantiate(_point, position, Quaternion.identity);
+            _points.Enqueue(tempPoint.position);
+            _lineRenderer.SetPosition(0, tempPoint.position);
+            _target = tempPoint.transform;
+            foreach (var ai in ListAI)
+            {
+                ai.EnqueueTarget(_target);
+            }
+            tempPoint.GetComponent<MovingPointController>().GetList(ListAI);
+        }
+
+        public void AddToList(AICharacterControl ai)
+        {
+            ListAI.Add(ai);
+        }
+    }
+}
